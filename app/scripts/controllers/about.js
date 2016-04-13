41acc135166099b125780d8d5c6cbc946614ff43
eXt-Ra <vanardois.romain@gmail.com>
2016-04-13 19:07:02 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dmscartoAngularGruntApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dmscartoAngularGruntApp
+ */
+angular.module('dmscartoAngularGruntApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
